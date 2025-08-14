f42974911f81ce6aafaf380c04d2642aeb1f098d
beatrice <btayl13@gmail.com>
2025-08-14 17:09:09 -0400

    works now :3

--- graph.py
+++ graph.py
@@ -7,12 +7,11 @@ from datetime import datetime
 import numpy as np
 import os
 import time
-import requests
+import requests_cache
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 import epd2in13_V4
 from PIL import Image,ImageDraw,ImageFont
-
-num_vars = 2
-make_plot = 1
 
 today = datetime.utcnow()
 year = today.strftime("%Y")
@@ -20,46 +19,57 @@ month = today.strftime("%m")
 #day = today.strftime("%d")
 day = '01'
 
-file0 = f"sci_xrsf-l2-avg1m_g18_d{year}{month}{day}_v2-2-0.nc"
+RETRIES = Retry(total=4, backoff_factor=2)
+SESSION = requests_cache.CachedSession('xraydata', expiers_after=1200)
+SESSION.mount('https://', HTTPAdapter(max_retries=RETRIES))
 
+
+file0 = f"sci_xrsf-l2-avg1m_g18_d{year}{month}{day}_v2-2-0.nc"
 url_path = f"https://data.ngdc.noaa.gov/platforms/solar-space-observing-satellites/goes/goes18/l2/data/xrsf-l2-avg1m_science/{year}/{month}/"
         
 if not os.path.exists(file0):
-    
-    headers = {"User-Agent": "SolarWeatherPi/1.0"}   
-    r = requests.get(url_path + file0, headers = headers, timeout=30)
-    
-    if r.status_code != 200:
-        raise RuntimeError(f"Download Failed {r.status_code}")
-    if len(r.content) < 100000:
-        raise RuntimeError("tiny file")
-    with open(file0,"wb") as f:
-        f.write(r.content)
-        print("got file")
-class graphvariables:
+
+    try:
+        headers = {"User-Agent": "SolarWeatherPi/1.0"}   
+        r = SESSION.get(url_path + file0, headers = headers, timeout=30)
+    except requests.exceptions.Timeout:
+        print("time out")
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP error: {e}")
+    else:
+        if r.status_code != 200:
+            raise RuntimeError(f"Download Failed {r.status_code}")
+        if len(r.content) < 100000:
+            raise RuntimeError("tiny file")
+        with open(file0,"wb") as f:
+            f.write(r.content)
+
+flareclasses = ["A","B", "C", "M", "X"]
+powersoften = [1e-8, 1e-7, 1e-6, 1e-5, 1e-4]
+
+class Graphvariables:
+
     def __init__(self):
         self.dd = xr.open_dataset(file0, engine="h5netcdf")
-        self.datetime0 = cftime.num2pydate(self.dd.variables["time"][:], self.dd["time"].units)
-        self.platform = getattr(self.dd, "platform")
-        self.var_name = ["xrsa_flux", "xrsb_flux"]
+        self.datetime0 = self.dd["time"]
+        self.var_name = self.dd["xrsa_flux"]
+        self.var_name2 = self.dd["xrsb_flux"]
 
 def makegraph():
-    if make_plot:
-        chan_color = ["mediumorchid", "green", "darkviolet", "indigo", "b",
-                      "darkcyan", "greenyellow", "yellow", "gold", "orange",
-                      "orangered", "darkred"][0:num_vars]
-    plt.figure(0, figsize=[2.75, 1.22])
-    g = graphvariables()
-    for ii in range(num_vars):
-        plt.plot(
-            g.datetime0[:],
-            g.dd[g.var_name[ii]].values,
-            linewidth=1,
-            color=chan_color[ii],
-            )
-        plt.yscale("log")
-        plt.savefig('xray.png')
-        print("made graph")
+    plt.figure(figsize=(3.75, 2.22), dpi=150)
+    g = Graphvariables()
+    plt.plot(g.datetime0.values, g.var_name.values, linewidth=1, color="black")
+    plt.plot(g.datetime0.values, g.var_name2.values, linewidth=1, color="black")
+    plt.tight_layout()
+    plt.yscale("log")
+    plt.gca().tick_params(axis='both', which='major', width=2, length=5, color="black")
+    plt.yticks(powersoften, flareclasses)
+    plt.gca().set_xticklabels([])
+    plt.grid(True, axis='y', linestyle='dotted', linewidth=2)
+    plt.savefig('xray.png', dpi=100, bbox_inches='tight')
+    plt.close()
+    print("made graph")
+
 epd = epd2in13_V4.EPD()
 
 def drawgraph():
@@ -71,12 +81,12 @@ def drawgraph():
     zImage = Image.new('1', (epd.height, epd.width), 10)
     draw = ImageDraw.Draw(zImage)
     graph = Image.open('xray.png')
-    g = graph.resize((275,120), Image.LANCZOS)
-    zImage.paste(g, (-4,-5))
-    draw.text((2, 20), "10^-7", font = font1, fill = 0) 
-    draw.text((2, 50), "10^-8", font = font1, fill = 0) 
-    draw.text((2, 90), "10^-9", font = font1, fill = 0) 
-    draw.text((70, 110), "Time[UT] 1 day period", font = font1, fill = 0) 
+    g = graph.resize((244, 100), Image.LANCZOS)
+    zImage.paste(g, (10, 10))
+    #draw.text((2, 20), "10^-7", font = font1, fill = 0) 
+    #draw.text((2, 50), "10^-8", font = font1, fill = 0) 
+    #draw.text((2, 90), "10^-9", font = font1, fill = 0) 
+    draw.text((70, 109), "Time[UT] 1 day period", font = font1, fill = 0) 
     epd.display(epd.getbuffer(zImage))
 
     print("displaying graph")
@@ -85,5 +95,5 @@ def drawgraph():
 
 def main():
     makegraph()
-    time.sleep(2)
+    time.sleep(0.1)
     drawgraph()
