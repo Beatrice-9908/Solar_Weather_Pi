d0e3e0cbd8c2e936e2052c5d8b47b23bbc8a9e0f
beatrice <btayl13@gmail.com>
2025-08-14 14:32:00 -0400

    fixin

--- graph.py
+++ graph.py
@@ -18,16 +18,24 @@ today = datetime.utcnow()
 year = today.strftime("%Y")
 month = today.strftime("%m")
 #day = today.strftime("%d")
-day = 11
+day = '01'
 
 file0 = f"sci_xrsf-l2-avg1m_g18_d{year}{month}{day}_v2-2-0.nc"
 
 url_path = f"https://data.ngdc.noaa.gov/platforms/solar-space-observing-satellites/goes/goes18/l2/data/xrsf-l2-avg1m_science/{year}/{month}/"
         
 if not os.path.exists(file0):
-        r = requests.get(url_path + file0, timeout=30) 
-        with open(file0,"wb") as f:
-            f.write(r.content)
+    
+    headers = {"User-Agent": "SolarWeatherPi/1.0"}   
+    r = requests.get(url_path + file0, headers = headers, timeout=30)
+    
+    if r.status_code != 200:
+        raise RuntimeError(f"Download Failed {r.status_code}")
+    if len(r.content) < 100000:
+        raise RuntimeError("tiny file")
+    with open(file0,"wb") as f:
+        f.write(r.content)
+        print("got file")
 class graphvariables:
     def __init__(self):
         self.dd = xr.open_dataset(file0, engine="h5netcdf")
@@ -40,19 +48,18 @@ def makegraph():
         chan_color = ["mediumorchid", "green", "darkviolet", "indigo", "b",
                       "darkcyan", "greenyellow", "yellow", "gold", "orange",
                       "orangered", "darkred"][0:num_vars]
-    plt.figure(0, figsize=[8, 5])
-    plt.subplots(figsize(2.5, 1.22), dpi=100)
+    plt.figure(0, figsize=[2.75, 1.22])
     g = graphvariables()
     for ii in range(num_vars):
         plt.plot(
             g.datetime0[:],
-            g.ff.variables[g.var_name[ii]][:],
+            g.dd[g.var_name[ii]].values,
             linewidth=1,
             color=chan_color[ii],
             )
         plt.yscale("log")
         plt.savefig('xray.png')
-
+        print("made graph")
 epd = epd2in13_V4.EPD()
 
 def drawgraph():
@@ -72,7 +79,7 @@ def drawgraph():
     draw.text((70, 110), "Time[UT] 1 day period", font = font1, fill = 0) 
     epd.display(epd.getbuffer(zImage))
 
-
+    print("displaying graph")
     epd.init()
     epd.sleep()
 
